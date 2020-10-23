ec96fdabc5feff1abe9f362b6ccba4ec6bd17c44
Garrett Breach <breachg1992@gmail.com>
2020-10-23 17:58:11 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,9 @@
 var playlist = {Elder: 'Sanctuary'};
 
 function updatePlaylist(playlist, artistName, songTitle){
- Object.assign({}, playlist, {['ACDC']: 'Back'})
+ Object.assign({}, playlist, {[key]: value})
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.Elder
 }
